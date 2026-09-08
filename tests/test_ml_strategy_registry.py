17d8e05bf8332e1016ac94c847ff6826af6d343e
kurnFR <kurniawan.liusvia@gmail.com>
2026-09-08 23:13:47 +0700

    test: align ML registry fixture with strategy name

--- tests/test_ml_strategy_registry.py
+++ tests/test_ml_strategy_registry.py
@@ -8,7 +8,7 @@ from ml.strategy_registry import available_ml_strategies, validate_base_strategy
 def make_artifact(model_id="m1", status_eligible=True):
     return ModelArtifact(
         model_id=model_id, experiment_id="e1", model_type="random_forest",
-        base_strategy="trend_ema", feature_columns=("rsi",),
+        base_strategy="trend_ema_v1", feature_columns=("rsi",),
         strategy_params={}, model_params={}, threshold=.6,
         train_metrics={}, validation_metrics={}, test_metrics={},
         eligible_for_paper=status_eligible,
@@ -21,7 +21,7 @@ class TestMLStrategyRegistry(unittest.TestCase):
         registry.add(make_artifact())
         menu = available_ml_strategies(registry)
         self.assertIn("ml:m1", menu)
-        self.assertEqual(menu["ml:m1"]["base_strategy"], "trend_ema")
+        self.assertEqual(menu["ml:m1"]["base_strategy"], "trend_ema_v1")
 
     def test_paper_menu_filters_research(self):
         registry = ModelRegistry()
@@ -29,7 +29,7 @@ class TestMLStrategyRegistry(unittest.TestCase):
         self.assertEqual(available_ml_strategies(registry, paper_only=True), {})
 
     def test_base_strategy_must_exist(self):
-        validate_base_strategy("trend_ema")
+        validate_base_strategy("trend_ema_v1")
         with self.assertRaises(ValueError):
             validate_base_strategy("does_not_exist")
 
