cc0ab226ce61c7b1588c318dd26a1e56f764cb6e
kurnFR <kurniawan.liusvia@gmail.com>
2026-09-13 11:08:32 +0700

    feat: export ML tournament runner APIs

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,20 +1,32 @@
 """Machine-learning research and strategy components."""
 
 from ml.experiment import ExperimentConfig
-from ml.trainer import TrainingResult, ThresholdResult, train_experiment, select_validation_threshold
+from ml.trainer import (
+    TrainingResult,
+    ThresholdResult,
+    train_experiment,
+    select_validation_threshold,
+    fit_experiment,
+    evaluate_locked_model,
+)
 from ml.runner import MLRunResult, run_experiment
 from ml.models import create_model, supported_models
 from ml.metrics import classification_metrics
+from ml.tournament_runner import TournamentResult, run_tournament
 
 __all__ = [
     "ExperimentConfig",
     "TrainingResult",
     "ThresholdResult",
-    "MLRunResult",
     "train_experiment",
     "select_validation_threshold",
+    "fit_experiment",
+    "evaluate_locked_model",
     "run_experiment",
+    "MLRunResult",
     "create_model",
     "supported_models",
     "classification_metrics",
+    "TournamentResult",
+    "run_tournament",
 ]
